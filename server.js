c83a22effbe881f199ff7d976d4922f5c55218e2
Elad Ohana <69722689+eohana-unc@users.noreply.github.com>
2022-01-24 19:47:54 -0500

    Added functionality to display index.html on request

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const http = require("http");
-// const fs = require("fs");
-// const minimist = require("minimist");
+const http = require("http")
+const fs = require("fs")
+// const minimist = require("minimist")
 
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
+  
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end('<h1>Hello, World!</h1>')
+  try {
+    const webpage = fs.readFileSync("www/index.html", "utf8")
+    res.end(webpage)
+  } catch (err) {
+    console.log(err)
+  } 
 })
 
 server.listen(port, () => {
