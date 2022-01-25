cd4dffc5533d0feef9932253d6f830c158e721ea
Elad Ohana <69722689+eohana-unc@users.noreply.github.com>
2022-01-24 19:07:11 -0500

    Set up and tested sample webserver

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+// const fs = require("fs");
+// const minimist = require("minimist");
+
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.end('<h1>Hello, World!</h1>')
+})
+
+server.listen(port, () => {
+  console.log(`Server running at port ${port}`)
+})
